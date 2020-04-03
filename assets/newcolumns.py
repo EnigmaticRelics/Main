125cfc2c96cc16686d46d4ae4fd41bb39abbfa4f
Bryce <bryce.marble@gmail.com>
2020-04-03 17:57:12 -0400

    updated python scripts and user database

---
+++ assets/newcolumns.py
@@ -0,0 +1,30 @@
+#used to add more columns
+import sqlite3
+
+sqlite_file = 'userdb.sqlite'
+table_name = 'users'
+id_column = 'primary key'
+new_column1 = 'email address'
+new_column2 = 'age'
+new_column3 = 'password'
+
+column_type = 'INTEGER'
+column_type2 = 'TEXT'
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=id_column, ct=column_type2))
+
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column1, ct=column_type2))
+
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column2, ct=column_type))
+
+c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
+        .format(tn=table_name, cn=new_column3, ct=column_type2))
+
+conn.commit()
+conn.close()
