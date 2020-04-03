f8e0315b251f77e0187fd60294160d1d2443b3ea
Bryce <bryce.marble@gmail.com>
2020-04-02 22:44:26 -0400

    added missing create user page. Also added database.py

---
+++ assets/database.py
@@ -0,0 +1,17 @@
+#This will by the python script that creates tables and inserts data into sqlite database
+
+import sqlite3
+
+sqlite_file = 'userdb.sqlite'
+table_name1 = 'users'
+new_field = 'username'
+field_type = 'TEXT'
+
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+c.execute('CREATE TABLE {tn} ({nf} {ft})'\
+        .format(tn=table_name1, nf=new_field, ft=field_type))
+
+conn.commit()
+conn.close()
